aa0be77e5c1f7ad35a1c4a67819f8d904636ceb5
Sergey Zarubin <s.zarubin.mail@gmail.com>
2019-03-13 10:34:08 +0300

    initial code

---
+++ FileExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileExample
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await File.WriteAllTextAsync("txt.txt", "Must write a file");
+        }
+    }
+}
