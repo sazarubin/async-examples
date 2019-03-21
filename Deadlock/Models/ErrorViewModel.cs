cca83f74566752bebfda130c541860402fd97a46
Sergey Zarubin <s.zarubin@dodopizza.com>
2019-03-21 22:45:25 +0300

    Deadlock web project now builds in net462

--- Deadlock/Models/ErrorViewModel.cs
+++ Deadlock/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Deadlock.Models
 {
     public class ErrorViewModel
--- Deadlock/Program.cs
+++ Deadlock/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Hosting;
 
 namespace Deadlock
 {
@@ -18,7 +10,8 @@ namespace Deadlock
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            new WebHostBuilder()
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
