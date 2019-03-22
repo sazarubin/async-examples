91e910561bb8d1bd00b3b1083c26a8e5e3be7c9c
Sergey Zarubin <s.zarubin.mail@gmail.com>
2019-03-22 12:09:44 +0300

    add examples

--- Async/Program.cs
+++ Async/Program.cs
@@ -3,17 +3,17 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace AsyncOnly
+namespace Async
 {
     internal static class Program
     {
         private static async Task Main(string[] args)
         {
-            var instance = new NonStatic();
+            var instance = new SomeClass();
             instance.VoidMethod();
             await instance.AsyncOnly();
             await instance.SingleAwait();await instance.TwoAwaits();
-            Task.Run(Console.WriteLine);
+            await Task.Run(Console.WriteLine);
             var factory = Task.Factory;
             var longRunning = factory.StartNew(Console.WriteLine,
                 factory.CreationOptions | TaskCreationOptions.LongRunning);
--- Async/SomeClass.cs
+++ Async/SomeClass.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Threading.Tasks;
 
-namespace AsyncOnly
+namespace Async
 {
-    public class NonStatic
+    public class SomeClass
     {
         public async void VoidMethod()
         {
@@ -28,6 +28,21 @@ namespace AsyncOnly
             await Task.Delay(10);
             Console.WriteLine("first await finished");
             await Task.Delay(10);
+            Console.WriteLine("second await finished");
+        }
+
+        public async Task TwoAwaitLoops()
+        {
+            Console.WriteLine("two awaits started");
+            for (var i = 0; i < 5; i++)
+            {
+                await Task.Delay(10);
+            }
+            Console.WriteLine("first await finished");
+            for (var j = 0; j < 3; j++)
+            {
+                await Task.Delay(10);
+            }
             Console.WriteLine("second await finished");
         }
     }
